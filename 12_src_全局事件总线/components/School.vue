a3f20ede1cab2d000fa3f1089c9d4de7bf350597
燕云少君 Eason <296786475@qq.com>
2023-11-20 16:08:29 +0800

    Todos全局事件总线

---
+++ 12_src_全局事件总线/components/School.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="demo">
+    <h2>学校名称： {{ name }}</h2>
+    <h2>学校地址： {{ address }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "北京大学",
+      address: "中国·北京",
+    };
+  },
+  mounted() {
+    this.$bus.$on('hello', (data) => {
+      console.log('School组件接收到了数据，', data);
+    })
+  },
+  beforeDestroy() {
+    this.$bus.off('hello');//销毁解绑
+  }
+};
+</script>
+
+<style scoped>
+.demo {
+  background: aquamarine;
+  padding: 5px;
+}
+</style>
---
+++ 12_src_全局事件总线/components/Student.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="demo">
+    <h2>姓名： {{ name }}</h2>
+    <h2>性别： {{ sex }}</h2>
+    <button type="button" @click="sendName">把学生名传递给School</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "Eason",
+      sex: "Male",
+    };
+  },
+  methods: {
+    sendName() {
+      console.log('sendName');
+      this.$bus.$emit('hello', this.name)
+    },
+  }
+};
+</script>
+
+<style scoped>
+.demo {
+  background: orange;
+  padding: 5px;
+  margin-bottom: 5px;
+}
+</style>
