e9563cc0f4898e8731a63f8df64076ea0d9b0e05
燕云少君 Eason <296786475@qq.com>
2023-11-23 14:12:58 +0800

    github搜索案例(vue-resource版本)

---
+++ 21_src_github搜索案例(vue-resource版本)/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <Search/>
+    <list/>
+  </div>
+</template>
+
+<script>
+import List from './components/List';
+import Search from './components/Search';
+
+export default {
+  name: "App",
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  components: {
+    Search,
+    List
+  },
+  methods: {
+    
+  },
+  
+};
+</script>
+
+<style>
+
+</style>
