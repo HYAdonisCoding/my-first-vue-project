79753d59741cc2e376e71f9ce78dc59aa6f7d47d
Eason <296786475@qq.com>
2023-11-29 18:13:30 +0800

    路由的基本使用

---
+++ 27_src_路由的基本使用/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>我是Home的内容</h2>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    console.log("Home is mounted");
+  },
+  beforeDestroy() {
+    console.log("Home is beforeDestroy");
+  },
+};
+</script>
+
+<style scoped>
+</style>
