4aeeafd27dee04e8f1d51ef43a105168f6446c90
Eason <296786475@qq.com>
2023-11-30 10:05:08 +0800

    多级路由

---
+++ 28_src_多级路由/pages/Message.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <a href="/message1">message001</a>&nbsp;&nbsp;
+      </li>
+      <li>
+        <a href="/message2">message002</a>&nbsp;&nbsp;
+      </li>
+      <li>
+        <a href="/message/3">message003</a>&nbsp;&nbsp;
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  mounted() {
+    console.log("Massage is mounted");
+  },
+  beforeDestroy() {
+    console.log("Massage is beforeDestroy");
+  },
+};
+</script>
+
+<style scoped>
+</style>
